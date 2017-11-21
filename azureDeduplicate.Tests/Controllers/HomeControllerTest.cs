6b38bdc2429972f166913901b869454130029f65
KirillovDenis <pushistiy96@mail.ru>
2017-11-21 17:27:12 +0300

    fixed unit test

--- azureDeduplicate.Tests/Controllers/HomeControllerTest.cs
+++ azureDeduplicate.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace azureDeduplicate.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.IsNotNull(null);
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]

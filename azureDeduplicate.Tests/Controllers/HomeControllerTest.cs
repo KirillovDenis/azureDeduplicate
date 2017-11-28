1462d8134ff4e00fce8d6cacfcf17e3cdb567434
KirillovDenis <pushistiy96@mail.ru>
2017-11-28 12:30:10 +0300

    commit that fails tests

--- azureDeduplicate.Tests/Controllers/HomeControllerTest.cs
+++ azureDeduplicate.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace azureDeduplicate.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(null)
         }
 
         
